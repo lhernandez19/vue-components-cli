e2660be0f8eb39f8710067812d30ea25d2537fcd
lhernandez19 <diegotapia1987@gmail.com>
2021-10-25 23:45:31 -0500

    working

--- src/models/LibraryBag.js
+++ src/models/LibraryBag.js
@@ -7,22 +7,9 @@ export default function LibraryBag(){
         this.push(
             new LibraryBagItem(
                 item,
-<<<<<<< HEAD
                 ((collection) => function(){
                     collection.removeItem(this)
                 })(this)));
-=======
-
-                ((collection) => function(){
-                    collection.removeItem(this)
-                })(this),
-
-                ((collection) => function(){
-                    collection.addItem(this)
-                })(this),
-            )
-        );
->>>>>>> 7167610770c84b041b6f64728b19ac3f754ca4be
 
         // allows for chaining
         // return this;
@@ -45,10 +32,4 @@ export default function LibraryBag(){
 
     return arr;
 
-<<<<<<< HEAD
 }
-=======
-export default LibraryBagCollection;
-
-
->>>>>>> 7167610770c84b041b6f64728b19ac3f754ca4be
--- src/models/LibraryBagItem.js
+++ src/models/LibraryBagItem.js
@@ -1,22 +1,7 @@
-<<<<<<< HEAD
 export default function LibraryBagItem(libraryItem, removeMethod){
     this.libraryItem = libraryItem;
 
     this.checkout = () => this.libraryItem.checkOut();
 
     this.remove = removeMethod || function(){};
-=======
-export default function BagItem (libraryItem, removeMethod){
-this.libraryItem = libraryItem;
-
-this.checkOut = () => this.libraryItem.checkOut();
-
-this.getTitle = () => this.libraryItem.title;
-
-
-
-
-this.remove = removeMethod || function(){};
-
->>>>>>> 7167610770c84b041b6f64728b19ac3f754ca4be
 }
